62ed2c9f69513506c1082ab4411801d0b73cb606
willpreebs <wpriebe16124@gmail.com>
2022-10-02 16:23:55 -0400

    added files from completion of tutorial

---
+++ index.js
@@ -0,0 +1,36 @@
+
+/*Express initializes app to be a function handler that you can supply to an HTTP server (as seen in line 4).
+We define a route handler / that gets called when we hit our website home.
+We make the http server listen on port 3000. */
+
+// require() is a node keyword that loads modules
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const { isObjectBindingPattern } = require('typescript');
+const server = http.createServer(app);
+// { } is a destructuring assignment. Now "Server" here will be automatically assigned to the "Server" object in the socket.io module
+const { Server } = require("socket.io")
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg)
+    });
+
+    
+  });
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+// io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+
+
